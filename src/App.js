18d4289be5e40a4734f862cbb922b8e49ca41856
HYHYJ <jjangdaram@naver.com>
2023-11-09 14:43:42 +0900

    recoil로 다크모드 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 // import React, { useState } from "react";
 import "./App.css";
 import Page from "./components/Page";
-import useStore from "./store.js";
+// import useStore from "./store.js";
+import { atom, useRecoilState } from "recoil";
 
 function App() {
-  const { isDark, setIsDark } = useStore((state) => state);
-  return <Page isDark={isDark} toggleThema={setIsDark} />;
+  let isDark = atom({
+    key: "isDark",
+    default: 0,
+  });
+
+  const [dark, setdark] = useRecoilState(isDark);
+
+  const setDarkFunction = (event) => {
+    setdark(!dark);
+  };
+  return <Page isDark={dark} toggleThema={setDarkFunction} />;
 }
 
 export default App;
